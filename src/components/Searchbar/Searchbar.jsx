87abfea5258c25d0d25523ae7ae8b520e93b15b9
Alla199707 <onoschenko20@gmail.com>
2023-04-09 21:22:11 +0300

    ++

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,51 +1,90 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import { Button, Form, Header, Input, Label } from './Searchbar.styled';
 
-class Search extends Component {
-  state = {
-    searchText: '',
+function Search({ createSearchText }) {
+  const [searchText, setSearchText] = useState('');
+
+  const handleChange = event => {
+    setSearchText(event.currentTarget.value.toLowerCase());
   };
 
-  handleChange = event => {
-    this.setState({ searchText: event.currentTarget.value.toLowerCase() });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (this.state.searchText.trim() === '') {
+    if (searchText.trim() === '') {
       alert('Please enter a request!');
       return;
     }
 
-    this.props.createSearchText(this.state.searchText);
-    this.setState({ searchText: '' });
+    createSearchText(searchText);
+    setSearchText('');
   };
 
-  render() {
-    return (
-      <Header>
-        <Form onSubmit={this.handleSubmit}>
-          <Button type="submit">
-            <Label>
-              <BsSearch />
-            </Label>
-          </Button>
-          <Input
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            onChange={this.handleChange}
-            value={this.state.searchText}
-          />
-        </Form>
-      </Header>
-    );
-  }
+  return (
+    <Header>
+      <Form onSubmit={handleSubmit}>
+        <Button type="submit">
+          <Label>
+            <BsSearch />
+          </Label>
+        </Button>
+        <Input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          onChange={handleChange}
+          value={searchText}
+        />
+      </Form>
+    </Header>
+  );
 }
+// class Search extends Component {
+//   state = {
+//     searchText: '',
+//   };
+
+//   handleChange = event => {
+//     this.setState({ searchText: event.currentTarget.value.toLowerCase() });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     if (this.state.searchText.trim() === '') {
+//       alert('Please enter a request!');
+//       return;
+//     }
+
+//     this.props.createSearchText(this.state.searchText);
+//     this.setState({ searchText: '' });
+//   };
+
+//   render() {
+//     return (
+//       <Header>
+//         <Form onSubmit={this.handleSubmit}>
+//           <Button type="submit">
+//             <Label>
+//               <BsSearch />
+//             </Label>
+//           </Button>
+//           <Input
+//             type="text"
+//             autoComplete="off"
+//             autoFocus
+//             placeholder="Search images and photos"
+//             onChange={this.handleChange}
+//             value={this.state.searchText}
+//           />
+//         </Form>
+//       </Header>
+//     );
+//   }
+// }
 
 Search.propTypes = {
   createSearchText: PropTypes.func.isRequired,
